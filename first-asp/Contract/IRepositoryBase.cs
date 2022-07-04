0c50f87c79fdf35d083280e4f1877410f3b9858f
AbdelrahmanHamza <sameh.hamza11@gmail.com>
2022-07-04 21:01:20 +0200

    create and edite leavetype controller

--- first-asp/Contract/IRepositoryBase.cs
+++ first-asp/Contract/IRepositoryBase.cs
@@ -6,6 +6,7 @@ namespace first_asp.Contract
     {
         ICollection<T> FindAll();
         T FindById(int id);
+        bool isExist(int id);
         bool Create(T entity);
         bool Update(T entity);
         bool Delete(T entity);
--- first-asp/Controllers/LeaveTypesController.cs
+++ first-asp/Controllers/LeaveTypesController.cs
@@ -46,7 +46,7 @@ namespace first_asp.Controllers
         // POST: LeaveTypesController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(LeaveType model)
+        public ActionResult Create(LeaveTypeVM model)
         {
             try
             {
@@ -66,28 +66,48 @@ namespace first_asp.Controllers
             }
             catch
             {
-                return View();
+                ModelState.AddModelError("", "something went wrong");
+                return View(model);
             }
         }
 
         // GET: LeaveTypesController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            if (!_repo.isExist(id))
+            {
+                return NotFound();
+            }
+            var leavetype = _repo.FindById(id);
+            var model = _mapper.Map<LeaveTypeVM>(leavetype);
+            return View(model);
         }
 
         // POST: LeaveTypesController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(LeaveTypeVM model)
         {
             try
             {
+                if (!ModelState.IsValid) 
+                {
+                    return View(model);                
+                }
+
+                var leavetype= _mapper.Map<LeaveType>(model);
+                var isSuccess= _repo.Update(leavetype);
+                if (!isSuccess) 
+                {
+                    ModelState.AddModelError("", "Something Went Wrong");
+                    return View(model);
+                }
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                ModelState.AddModelError("","Something Went Wrong");
+                return View(model);
             }
         }
 
